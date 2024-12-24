9aa470036312c45f0de84d9e977bcf9452f28d20
Xiong Kuan <fudjdj@icloud.com>
2024-12-24 22:03:10 +0800

    add the vue-bind, vue-if, vue-elseif, vue-else etc.

--- system/src/views/Home.vue
+++ system/src/views/Home.vue
@@ -6,13 +6,32 @@ import {reactive, ref} from "vue";
 
   const data = reactive({
     a: 123,
-    b: "Every moment is a new beginning."
+    b: "Every moment is a new beginning.",
+    name:"佩奇",
+    arr:[7,8,9],
+    fruit:['apple','banana','orange'],
+    language:['中文'],
+    box: {
+      width: '200px',
+      height: '200px',
+      color: 'red'
+    },
+    image: 'https://rusttalk.github.io/misc/xyz-qrcode.webp'
   })
+  const click = () => {
+  alert("keep it going");
+}
+
+  const show = (value) => {
+    alert(value)
+  }
+
 </script>
+
 
 <template>
   <div>
-    <div style="background-color: red">
+    <div style="background-color: darkorange">
       welcome to the home page
     </div>
 
@@ -30,9 +49,31 @@ import {reactive, ref} from "vue";
       <input v-model="data.a"/>
     </div>
 
+    <div style="margin-bottom: 20px">
+      <span style="color: red" v-if="data.name === '佩奇'">小猪佩奇</span>
+      <span style="color: blue" v-else-if="data.name ==='乔治'">小猪乔治</span>
+      <span style="color: aqua" v-else>Larry Page</span>
+    </div>
+
+    <div style="display: flex; ">
+      <div @click="show(item)" style="width: 300px; height: 300px; background-color: aqua; text-align: center; font-size: 30px; line-height: 300px; margin-right: 30px; margin-bottom: 30px" v-for="item in data.arr">{{ item }}</div>
+    </div>
+
     <div>
-      <span style="color: red">小猪佩奇</span>
-      <span style="color: blue">小猪乔治</span>
+      <select style="width: 200px; margin-bottom: 20px">
+          <option v-for="item in data.fruit">{{ item }}</option>
+      </select>
+    </div>
+
+    <div style="margin-bottom: 20px">
+      <button v-on:click="click">click me</button>
+    </div>
+
+    <div>
+      <div :style="data.box"></div>
+      <div>
+        <img :src="data.image" alt="">
+      </div>
     </div>
   </div>
 
